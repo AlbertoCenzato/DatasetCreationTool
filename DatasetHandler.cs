a2c913c0e0d0d9d93ed08728dc6506bb16ea4948
Alberto Cenzato <alberto.cenzato@it-robotics.it>
2020-07-24 15:16:13 +0200

    Remove unused usings

--- DatasetHandler.cs
+++ DatasetHandler.cs
@@ -4,11 +4,8 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Windows.Forms.VisualStyles;
-using System.Xml;
 
 namespace DatasetCreationTool
 {
